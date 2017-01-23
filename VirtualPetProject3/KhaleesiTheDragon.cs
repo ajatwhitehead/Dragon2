f06e53c208e6c1a21714b22063c45d3049b37e4c
Aja Whitehead <aja.whitehead1@gmail.com>
2017-01-23 08:52:55 -0500

    first commit

---
+++ VirtualPetProject3/KhaleesiTheDragon.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetProject3
+{
+    class KhaleesiTheDragon
+    {
+
+        private int hunger;
+        private int mood;
+        private int activity;
+        //fields//
+
+
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Mood
+        {
+            get { return this.mood; }
+            set { this.mood = value; }
+        }
+
+        public int Activity
+        {
+            get { return this.activity; }
+            set { this.activity = value; }
+        }
+        //Properties//
+
+        public KhaleesiTheDragon(int hunger, int mood, int activity)
+        {
+            this.hunger = hunger;
+            this.mood = mood;
+            this.activity = activity;
+        }
+
+
+
+        //methods//
+
+        public void khaleesiTheDragon()
+        {
+            Console.WriteLine("hunger:" + hunger);
+            Console.WriteLine("mood:" + mood);
+            Console.WriteLine("activity:" + activity);
+        }
+        public void ChoicesForK()
+        {
+           
+            {
+                Console.WriteLine("a. Do you want to feed me?");
+                Console.WriteLine("b. Do you want to play with me?");
+                Console.WriteLine("c. Do you want to know how I feel?");
+
+                string result = Console.ReadLine();
+
+                if (result == "a")
+                {
+                    Console.WriteLine(" Thank you I was so hungry I could eat a Tree");
+                   
+                }
+
+
+                else if (result == "b")
+                {
+                    Console.WriteLine("Do you want to play with fire?");
+                }
+
+                else if (result == "c")
+                {
+                    Console.WriteLine("I am HAPPY you are my mom!");
+                }
+                else 
+                {
+                    Console.WriteLine("If you don't want to do anything with me then I will go and feed myself");
+                }
+
+            }
+
+        }
+    }
+}
+
+
---
+++ VirtualPetProject3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPetProject3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mommy! I am Khaleesi your new Baby Dragon.");
+            Console.WriteLine("I am going to tell you what I need to take care of me and how I can take care of myself.");
+
+            KhaleesiTheDragon pet1 = new KhaleesiTheDragon(45, 72, 65);
+            pet1.khaleesiTheDragon();
+
+            
+            {
+                Console.WriteLine("Hey Mommy! What do you want to do with me? ");
+
+                pet1.ChoicesForK();
+            }
+
+        }
+    }
+    }
+
